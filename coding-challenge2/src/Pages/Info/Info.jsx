8d0f34d87b8c2880290e3bdb160d6e28e5b9b575
KeenanKistoo <kistook19@gmail.com>
2024-04-18 13:50:53 +0200

    Updated Quantity
    Previously, the info page did not have a quantity input field. Updated that now

--- coding-challenge2/src/Pages/Info/Info.jsx
+++ coding-challenge2/src/Pages/Info/Info.jsx
@@ -6,9 +6,6 @@ function Info(props){
 
     const {id, desc, duration, stops, price} = props;
 
-    function backButton(){
-        <Link to={'/'}></Link>
-    }
     return(
         <>
         <article className="info-sect">
@@ -18,9 +15,14 @@ function Info(props){
             <p className="stops">{"Total Number of Stops: " + stops}</p>
             <p className="price-info">{"Total Price: " + "R" + price}</p>
         </article>
+        <section className="input-sect">
+            <button className="change-btn">-</button>
+            <input className="input" type="text" />
+            <button className="change-btn">+</button>
+        </section>
         <section className="purchase-sect">
+            <Link to={'/'} className="back-btn">Back To Routes</Link>
             <button className="pur-btn">Add Ticket/s To Cart</button>
-            <Link to={'/'} className="title">Back To Routes</Link>
         </section>
         </>
     );
