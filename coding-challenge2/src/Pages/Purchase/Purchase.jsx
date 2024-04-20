712d3185c4070901fd8f9279fe42cf45f43f536c
KeenanKistoo <kistook19@gmail.com>
2024-04-20 21:01:35 +0200

    Added Reflection

--- coding-challenge2/src/Pages/Purchase/Purchase.jsx
+++ coding-challenge2/src/Pages/Purchase/Purchase.jsx
@@ -56,3 +56,25 @@ function Purchase(){
 }
 
 export default Purchase;
+
+/**Reflection:
+ * For this challenge, I think that I tried to create this overlap between the class
+ * exercise memo and the challenge's requirements. Through most part, I was successful
+ * however, I felt the need to constantly question why certain aspects were done, 
+ * because I simply did not understand. 
+ * 
+ * Keeping this in mind, I spent a lot of the time breaking down and overanalysing my 
+ * code to improve this already efficient system from the exercise. 
+ * 
+ * I decided to break down each aspect in a flowchart to show where the context overlaps.
+ * This made the process significantly easier, as I was no longer trying to just replicate
+ * what I had to match the new dataset.
+ * 
+ * Also, routing was unexpectedly frustrating considering the dataset was based of 'routes' XD.
+ * 
+ * Overall, I do think that I need to create basic react-apps to solidify my understanding on
+ * how everything works and find the quickest way to acheive certain goals. I felt that I could
+ * have used more components to clean up my code especially in Purchase.jsx. If I come back to this
+ * code in a couple of months for example, the code will look very saturated with a lot of code 
+ * which could have been broken down more. 
+ */
